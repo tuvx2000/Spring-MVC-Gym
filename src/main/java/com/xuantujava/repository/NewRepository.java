bf0c045ffa1c33338350bf59fa9b62a2546ace33
TUVX <tuhaha900@gmail.com>
2021-09-29 11:05:51 +0700

    Spring DATA JPA Basic

--- src/main/java/com/xuantujava/repository/NewRepository.java
+++ src/main/java/com/xuantujava/repository/NewRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.xuantujava.entity.NewEntity;
 
-public interface NewRepository extends JpaRepository<NewEntity, Long>{
-
+public interface NewRepository extends JpaRepository<NewEntity, Long> {
+	
 }
