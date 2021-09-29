e0d31bb62c4b322b44cd061d49fc5a5f20a28683
TUVX <tuhaha900@gmail.com>
2021-09-29 10:18:18 +0700

    Bug Tomcat pending

---
+++ src/main/java/com/xuantujava/repository/NewRepository.java
@@ -0,0 +1,9 @@
+package com.xuantujava.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.xuantujava.entity.NewEntity;
+
+public interface NewRepository extends JpaRepository<NewEntity, Long>{
+
+}
