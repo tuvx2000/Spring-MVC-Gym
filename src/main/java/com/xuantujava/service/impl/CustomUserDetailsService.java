98b4a71e9db60ea177b76e2d7236261d7df3231d
TUVX <tuhaha900@gmail.com>
2021-10-02 16:13:45 +0700

    Custom User

--- src/main/java/com/xuantujava/service/impl/CustomUserDetailsService.java
+++ src/main/java/com/xuantujava/service/impl/CustomUserDetailsService.java
@@ -1,38 +1,47 @@
 package com.xuantujava.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.xuantujava.DTO.NewDTO;
+import com.xuantujava.DTO.MyUser;
 import com.xuantujava.constant.SystemConstant;
-import com.xuantujava.dao.INewDAO;
-import com.xuantujava.dao.impl.NewDAO;
+import com.xuantujava.entity.RoleEntity;
 import com.xuantujava.entity.UserEntity;
-import com.xuantujava.repository.NewRepository;
-import com.xuantujava.repository.UserRepository;
 
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 	
-	
-//	@Autowired
-//	private UserRepository userRepository;
+//	
+////	@Autowired
+////	private UserRepository userRepository;
 
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//
-//		UserEntity userEntity = userRepository.findOneByUserNameAndStatus(username,SystemConstant.ACTIVE_STATUS);
-//		
-//		if(userEntity == null) {
-//			throw new UsernameNotFoundException("User not found");
-//		}
+
+		UserEntity userEntity = userRepository.findOneByUserNameAndStatus(username,SystemConstant.ACTIVE_STATUS);
+		
+		if(userEntity == null) {
+			throw new UsernameNotFoundException("User not found");
+		}
+		
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		for (RoleEntity role: userEntity.getRoles()) {
+			authorities.add(new SimpleGrantedAuthority(role.getCode()));
+		}
 		
 		//push thong tin vao security khi user login vao he thong
+		MyUser myUser  = new MyUser(userEntity.getUserName(), userEntity.getPassWord(), 
+				true, true, true, true, authorities);
+		myUser.setFullName(userEntity.getFullName());
 		
 		return null;
 	}
