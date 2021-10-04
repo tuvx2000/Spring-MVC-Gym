42d27de1834943085b928c76ec6c9892d2d8645d
TUVX <tuhaha900@gmail.com>
2021-10-04 08:37:02 +0700

    Change pom target

--- src/main/java/com/xuantujava/repository/UserRepository.java
+++ src/main/java/com/xuantujava/repository/UserRepository.java
@@ -1,16 +1,13 @@
 package com.xuantujava.repository;
 
-import java.util.List;
-
-import javax.persistence.Entity;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import com.xuantujava.entity.UserEntity;
+import com.xuantujava.entity.NewEntity;
+
+//import com.xuantujava.entity.UserEntity;
 
 
 //@Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-	UserEntity findOneByUserNameAndStatus(String name, int status);
+public interface UserRepository extends JpaRepository<NewEntity, Long> {
+//	UserEntity findOneByUserNameAndStatus(String name, int status);
 }
