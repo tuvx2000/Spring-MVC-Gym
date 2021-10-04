e8c39112ee41c5026d2c4bca4916eb65fc631077
TUVX <tuhaha900@gmail.com>
2021-10-04 08:37:02 +0700

    Revert "Change pom target"
    This reverts commit 42d27de1834943085b928c76ec6c9892d2d8645d.

--- src/main/java/com/xuantujava/service/impl/CustomUserDetailsService.java
+++ src/main/java/com/xuantujava/service/impl/CustomUserDetailsService.java
@@ -1,10 +1,20 @@
 package com.xuantujava.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.xuantujava.DTO.MyUser;
+import com.xuantujava.constant.SystemConstant;
+import com.xuantujava.entity.RoleEntity;
+import com.xuantujava.entity.UserEntity;
 import com.xuantujava.repository.UserRepository;
 
 
@@ -19,22 +29,22 @@ public class CustomUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-//		UserEntity userEntity = userRepository.findOneByUserNameAndStatus(username,SystemConstant.ACTIVE_STATUS);
-//		
-//		if(userEntity == null) {
-//			throw new UsernameNotFoundException("User not found");
-//		}
-//		
-//		List<GrantedAuthority> authorities = new ArrayList<>();
-//		for (RoleEntity role: userEntity.getRoles()) {
-//			authorities.add(new SimpleGrantedAuthority(role.getCode()));
-//		}
-//		
-//		//push thong tin vao security khi user login vao he thong
-//		MyUser myUser  = new MyUser(userEntity.getUserName(), userEntity.getPassWord(), 
-//				true, true, true, true, authorities);
-//		myUser.setFullName(userEntity.getFullName());
-//		
+		UserEntity userEntity = userRepository.findOneByUserNameAndStatus(username,SystemConstant.ACTIVE_STATUS);
+		
+		if(userEntity == null) {
+			throw new UsernameNotFoundException("User not found");
+		}
+		
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		for (RoleEntity role: userEntity.getRoles()) {
+			authorities.add(new SimpleGrantedAuthority(role.getCode()));
+		}
+		
+		//push thong tin vao security khi user login vao he thong
+		MyUser myUser  = new MyUser(userEntity.getUserName(), userEntity.getPassWord(), 
+				true, true, true, true, authorities);
+		myUser.setFullName(userEntity.getFullName());
+		
 		return null;
 	}
 
