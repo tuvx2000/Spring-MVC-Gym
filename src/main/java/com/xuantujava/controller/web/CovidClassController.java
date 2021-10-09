fb2ebdbbfe37f92c7d7c4825ec6d6064f556818a
TUVX <tuhaha900@gmail.com>
2021-10-09 16:11:10 +0700

    Basic API scrape from Youtube

--- src/main/java/com/xuantujava/controller/web/CovidClassController.java
+++ src/main/java/com/xuantujava/controller/web/CovidClassController.java
@@ -9,13 +9,128 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.xuantujava.model.UserModel;
 
+/////////////// Youtube API scrape comment
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.Data;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.CommentThread;
+import com.google.api.services.youtube.model.CommentThreadListResponse;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+
+
 
 @Controller(value ="covidclassControllerOfWeb")
 public class CovidClassController {
-
+/////////////// Youtube API scrape comment
 	
 
+	// You need to set this value for your code to compile.
+    // For example: ... DEVELOPER_KEY = "YOUR ACTUAL KEY";
+    private static final String DEVELOPER_KEY = "AIzaSyCHT6l7wJT9egYjkwenfzQHhmpHKE5HPUQ";
 
+    private static final String APPLICATION_NAME = "Gym Center";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+    /**
+     * Build and return an authorized API client service.
+     *
+     * @return an authorized API client service
+     * @throws GeneralSecurityException, IOException
+     */
+    public static YouTube getService() throws GeneralSecurityException, IOException {
+        final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        return new YouTube.Builder(httpTransport, JSON_FACTORY, null)
+            .setApplicationName(APPLICATION_NAME)
+            .build();
+    }
+
+	
+	
+    public static void main(String[] args)
+            throws GeneralSecurityException, IOException, GoogleJsonResponseException {
+            YouTube youtubeService = getService();
+            // Define and execute the API request
+            List<String> listCriteria = Arrays.asList(new String[]{"id","snippet"});
+            
+            
+            YouTube.CommentThreads.List request = youtubeService.commentThreads()
+               .list(listCriteria);
+            CommentThreadListResponse response = request.setKey(DEVELOPER_KEY)
+            	.setOrder("time")
+                .setVideoId("5wwZCZcHiDs")
+                .execute();
+            
+            
+            List<CommentThread> listComments = response.getItems();
+            
+            for(CommentThread comment :listComments)
+            	System.out.println(comment.getSnippet().getTopLevelComment().getSnippet().getTextDisplay());
+           
+
+        }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+/////////////// Youtube API scrape comment
 	@RequestMapping(value = "/covid-class", method = RequestMethod.GET)
 	public ModelAndView homePage(HttpServletRequest request) {
 		Long categoryId = 1L; 
--- src/main/java/com/xuantujava/controller/web/HomeController.java
+++ src/main/java/com/xuantujava/controller/web/HomeController.java
@@ -53,7 +53,6 @@ import com.amazonaws.services.comprehend.model.DetectSentimentRequest;
 import com.amazonaws.services.comprehend.model.DetectSentimentResult;
 ///end ////comprehened imports
 
-
 ////google SignIn
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
@@ -61,176 +60,139 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 
-
-
-
-
-@Controller(value ="homeControllerOfWeb")
+@Controller(value = "homeControllerOfWeb")
 public class HomeController {
 
 	@Autowired
 	ICategoryService categoryService;
-	
+
 	@Autowired
 	INewService newService;
-	
+
 	@Autowired
 	IUserService userService;
-	
+
 	@Autowired
 	ManagementGoogleUserService managementGoogleUserService;
-	
-	
-	///////////////////////////////////////////////////////////////////////////////////////////for comprehened
-			@RequestMapping(value = "/comprehened", method = RequestMethod.GET)
-			public ModelAndView comprehenedPage(HttpServletRequest request, HttpServletResponse response) {
-				//start comprehened
-				
-				 String text = "oh my shiet!!!this cousse is wort";	
-				
-				 AWSCredentialsProvider awsCreds = DefaultAWSCredentialsProviderChain.getInstance();
-				 
-			        AmazonComprehend comprehendClient =
-			            AmazonComprehendClientBuilder.standard()
-			                                         .withCredentials(awsCreds)
-			                                         .withRegion("ap-northeast-1")
-			                                         .build();
-			                                         
-			   // Call detectSentiment API
-			        System.out.println("Calling DetectSentiment");
-			        DetectSentimentRequest detectSentimentRequest = new DetectSentimentRequest().withText(text)
-			                                                                                    .withLanguageCode("en");
-			        DetectSentimentResult detectSentimentResult = comprehendClient.detectSentiment(detectSentimentRequest);
-			        System.out.println(detectSentimentResult);
-			        System.out.println("End of DetectSentiment\n");
-			        System.out.println( "Done" );
-				
-				////// end
-				
-				NewsModel newsModel = new NewsModel();
-				newsModel.setTitle(detectSentimentResult.toString());
 
-				request.setAttribute("model1",newsModel);
-				
-				
-				
-				
-				ModelAndView mav = new ModelAndView("web/home");
-				return mav;
-				}	
-	
-	
+	/////////////////////////////////////////////////////////////////////////////////////////// for
+	/////////////////////////////////////////////////////////////////////////////////////////// comprehened
+	@RequestMapping(value = "/comprehened", method = RequestMethod.GET)
+	public ModelAndView comprehenedPage(HttpServletRequest request, HttpServletResponse response) {
+		// start comprehened
+
+		String text = "oh my shiet!!!this cousse is wort";
+
+		AWSCredentialsProvider awsCreds = DefaultAWSCredentialsProviderChain.getInstance();
+
+		AmazonComprehend comprehendClient = AmazonComprehendClientBuilder.standard().withCredentials(awsCreds)
+				.withRegion("ap-northeast-1").build();
+
+		// Call detectSentiment API
+		System.out.println("Calling DetectSentiment");
+		DetectSentimentRequest detectSentimentRequest = new DetectSentimentRequest().withText(text)
+				.withLanguageCode("en");
+		DetectSentimentResult detectSentimentResult = comprehendClient.detectSentiment(detectSentimentRequest);
+		System.out.println(detectSentimentResult);
+		System.out.println("End of DetectSentiment\n");
+		System.out.println("Done");
+
+		////// end
+
+		NewsModel newsModel = new NewsModel();
+		newsModel.setTitle(detectSentimentResult.toString());
+
+		request.setAttribute("model1", newsModel);
+
+		ModelAndView mav = new ModelAndView("web/home");
+		return mav;
+	}
+
 	//////////////////////////////////////////////////////////////// google sign-in
 
 	@RequestMapping(value = "/google-dang-nhap", method = RequestMethod.POST)
-	public ModelAndView googleSignInPage(HttpServletRequest request, HttpServletResponse response) throws GeneralSecurityException, IOException {
+	public ModelAndView googleSignInPage(HttpServletRequest request, HttpServletResponse response)
+			throws GeneralSecurityException, IOException {
 
-		request = managementGoogleUserService.GoogleSignInAndRegister(request,response);
-		
+		request = managementGoogleUserService.GoogleSignInAndRegister(request, response);
+
 		ModelAndView mav = new ModelAndView("GoogleLogingOnLoading");
-		
+
 		return mav;
 	}
 	/////////////////////////////////////////////////////
 
-
 	@RequestMapping(value = "/trang-chu", method = RequestMethod.GET)
 	public ModelAndView homePage(HttpServletRequest request) {
-		
+
 		String title = "bai viet so 5";
-		String content ="bai viet so 5";
-		Long categoryId = 1L;	
-		
+		String content = "bai viet so 5";
+		Long categoryId = 1L;
+
 		NewsModel newsModel = new NewsModel();
-		newsModel.setTitle(request.getParameter("googleSignIn")+request.getParameter("googleSignIn1"));
+		newsModel.setTitle(request.getParameter("googleSignIn") + request.getParameter("googleSignIn1"));
 		newsModel.setContent(content);
 		newsModel.setCategoryId(categoryId);
-		String x = request.getParameter("googleSignIn")+request.getParameter("googleSignIn1");
-		//newService.save(newsModel);
-		request.setAttribute("model",newsModel);
-		
+		String x = request.getParameter("googleSignIn") + request.getParameter("googleSignIn1");
+		// newService.save(newsModel);
+		request.setAttribute("model", newsModel);
+
 		ModelAndView mav = new ModelAndView("web/home");
 		return mav;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	@RequestMapping(value = "/dang-nhap", method = RequestMethod.GET)
 	public ModelAndView loginPage(HttpServletRequest request, HttpServletResponse response) {
-		
+
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		if( !(auth instanceof AnonymousAuthenticationToken) ) {
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
 			ModelAndView mav = new ModelAndView("redirect:/trang-chu");
 			return mav;
-		}	
-		
-		
-		
-		
-		
+		}
 
 		ModelAndView mav = new ModelAndView("login");
 		return mav;
 	}
-	
+
 	@RequestMapping(value = "/dang-ky", method = RequestMethod.GET)
 	public ModelAndView registrationPage(HttpServletRequest request, HttpServletResponse response) {
-		
+
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		if( !(auth instanceof AnonymousAuthenticationToken) ) {
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
 			ModelAndView mav = new ModelAndView("redirect:/trang-chu");
 			return mav;
-		}		
-		
+		}
 
 		ModelAndView mav = new ModelAndView("registration");
 		return mav;
 	}
-	
-	
+
 	@RequestMapping(value = "/dang-ky", method = RequestMethod.POST)
 	public ModelAndView registrationProcess(HttpServletRequest request) {
-		
+
 		UserDTO userDTO = new UserDTO();
 		userDTO.setUserName(request.getParameter("j_username"));
 		userDTO.setPassword(request.getParameter("j_password"));
 		userDTO.setFullName(request.getParameter("j_fullname"));
-		
+
 		userService.RegisterNewUser(userDTO);
-		
 
 		ModelAndView mav = new ModelAndView("redirect:/dang-nhap");
 		return mav;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	@RequestMapping(value = "/dang-xuat", method = RequestMethod.GET)
 	public ModelAndView logoutPage(HttpServletRequest request, HttpServletResponse response) {
-		
+
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		if( auth != null) {
+		if (auth != null) {
 			new SecurityContextLogoutHandler().logout(request, response, auth);
 		}
 
 		ModelAndView mav = new ModelAndView("redirect:/trang-chu");
 		return mav;
 	}
-	
+
 	@RequestMapping(value = "/accessDenied", method = RequestMethod.GET)
 	public ModelAndView accessDenied() {
 
@@ -238,37 +200,4 @@ public class HomeController {
 		return mav;
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-		
 }
-
-
