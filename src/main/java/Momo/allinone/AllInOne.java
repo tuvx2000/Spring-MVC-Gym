0de803d8ec5190311644d24a44dccd77affab82a
TUVX <tuhaha900@gmail.com>
2021-10-23 21:38:10 +0700

    Momo Payment First

---
+++ src/main/java/Momo/allinone/AllInOne.java
@@ -0,0 +1,53 @@
+package Momo.allinone;
+
+import Momo.allinone.models.CaptureMoMoResponse;
+import Momo.allinone.models.PayGateResponse;
+import Momo.allinone.models.QueryStatusTransactionResponse;
+import Momo.allinone.processor.allinone.CaptureMoMo;
+import Momo.allinone.processor.allinone.PaymentResult;
+import Momo.allinone.processor.allinone.QueryStatusTransaction;
+import Momo.shared.utils.LogUtils;
+import Momo.shared.sharedmodels.Environment;
+
+/**
+ * Demo
+ */
+public class AllInOne {
+
+    /***
+     * Select environment
+     * You can load config from file
+     * MoMo only provide once endpoint for each envs: dev and prod
+     * @param args
+     * @throws
+     */
+
+    public static void main(String... args) throws Exception {
+        LogUtils.init();
+        String requestId = String.valueOf(System.currentTimeMillis());
+        String orderId = String.valueOf(System.currentTimeMillis());
+        long amount = 50000;
+
+        String orderInfo = "Pay With MoMo";
+        String returnURL = "https://google.com.vn";
+        String notifyURL = "https://google.com.vn";
+        String extraData = "";
+        String bankCode = "SML";
+
+        Environment environment = Environment.selectEnv("dev", Environment.ProcessType.PAY_GATE);
+
+
+//      Remember to change the IDs at enviroment.properties file
+
+//        Payment Method- Phương thức thanh toán
+        CaptureMoMoResponse captureMoMoResponse = CaptureMoMo.process(environment, orderId, requestId, Long.toString(amount), orderInfo, returnURL, notifyURL, "");
+
+//        Transaction Query - Kiểm tra trạng thái giao dịch
+        QueryStatusTransactionResponse queryStatusTransactionResponse = QueryStatusTransaction.process(environment, orderId, requestId);
+
+//      Process Payment Result - Xử lý kết quả thanh toán
+        PayGateResponse payGateResponse = PaymentResult.process(environment,new PayGateResponse());
+
+    }
+
+}
