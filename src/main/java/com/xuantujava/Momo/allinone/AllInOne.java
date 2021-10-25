69bbb0c1e4ca693bae07d2d7b09e0eb8852206f9
TUVX <tuhaha900@gmail.com>
2021-10-25 21:33:53 +0700

    Momo Finished one Direct

--- src/main/java/com/xuantujava/Momo/allinone/AllInOne.java
+++ src/main/java/com/xuantujava/Momo/allinone/AllInOne.java
@@ -1,57 +1,101 @@
-package  com.xuantujava.Momo.allinone;
+package com.xuantujava.Momo.allinone;
 
-import  com.xuantujava.Momo.allinone.models.CaptureMoMoResponse;
-import  com.xuantujava.Momo.allinone.models.PayGateResponse;
-import  com.xuantujava.Momo.allinone.models.QueryStatusTransactionResponse;
-import  com.xuantujava.Momo.allinone.processor.allinone.CaptureMoMo;
-import  com.xuantujava.Momo.allinone.processor.allinone.PaymentResult;
-import  com.xuantujava.Momo.allinone.processor.allinone.QueryStatusTransaction;
-import  com.xuantujava.Momo.shared.utils.LogUtils;
-import  com.xuantujava.Momo.shared.sharedmodels.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.xuantujava.Momo.allinone.models.CaptureMoMoResponse;
+import com.xuantujava.Momo.allinone.models.PayGateResponse;
+import com.xuantujava.Momo.allinone.models.QueryStatusTransactionResponse;
+import com.xuantujava.Momo.allinone.processor.allinone.CaptureMoMo;
+import com.xuantujava.Momo.allinone.processor.allinone.PaymentResult;
+import com.xuantujava.Momo.allinone.processor.allinone.QueryStatusTransaction;
+import com.xuantujava.Momo.shared.utils.LogUtils;
+import com.xuantujava.service.IUserService;
+import com.xuantujava.Momo.shared.sharedmodels.Environment;
 
 /**
  * Demo
  */
 public class AllInOne {
 
-    /***
-     * Select environment
-     * You can load config from file
-     * MoMo only provide once endpoint for each envs: dev and prod
-     * @param args
-     * @throws
-     */
+	/***
+	 * Select environment You can load config from file MoMo only provide once
+	 * endpoint for each envs: dev and prod @param args @throws
+	 */
+	
+	@Autowired
+	IUserService userService;
+	public static String dump(String userId) {
+		LogUtils.init();
+		
+		
+		
+		String requestId = String.valueOf(userId + "N" +System.currentTimeMillis());
+		String orderId = String.valueOf(userId + "N" +System.currentTimeMillis());
+		long amount = 50000;
 
-    public static void main(String... args) throws Exception {
-        LogUtils.init();
-        String requestId = String.valueOf(System.currentTimeMillis());
-        String orderId = String.valueOf(System.currentTimeMillis());
-        long amount = 50000;
+		String orderInfo = "Pay With MoMo";
+		String returnURL = "http://localhost:8080/spring-mvc/momo1";
+		String notifyURL = "http://localhost:8080/spring-mvc/momo2";
+		String extraData = "";
+		String bankCode = "SML";
 
-        String orderInfo = "Pay With MoMo";
-        String returnURL = "http://localhost:8080/spring-mvc/momo1";
-        String notifyURL = "http://14.245.171.225:8080/spring-mvc/momo2";
-        String extraData = "";
-        String bankCode = "SML";
+		Environment environment = Environment.selectEnv("dev", Environment.ProcessType.PAY_GATE);
 
-        Environment environment = Environment.selectEnv("dev", Environment.ProcessType.PAY_GATE);
+//	      Remember to change the IDs at enviroment.properties file
 
+//	        Payment Method- Phương thức thanh toán
+		CaptureMoMoResponse captureMoMoResponse;
+		try {
+			captureMoMoResponse = CaptureMoMo.process(environment, orderId, requestId, Long.toString(amount), orderInfo,
+					returnURL, notifyURL, "");
+			
+			System.out.println("1------------------------------------------------");
+
+			System.out.println("xxxPAYURL:  " + captureMoMoResponse.getPayUrl());
+			System.out.println("2------------------------------------------------");
+
+			
+			return captureMoMoResponse.getPayUrl().toString();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "";
+
+	}
+
+	public static void main(String... args) throws Exception {
+		LogUtils.init();
+		String requestId = String.valueOf(System.currentTimeMillis());
+		String orderId = String.valueOf(System.currentTimeMillis());
+		long amount = 50000;
+
+		String orderInfo = "Pay With MoMo";
+		String returnURL = "http://localhost:8080/spring-mvc/momo1";
+		String notifyURL = "http://14.245.171.225:8080/spring-mvc/momo2";
+		String extraData = "";
+		String bankCode = "SML";
+
+		Environment environment = Environment.selectEnv("dev", Environment.ProcessType.PAY_GATE);
 
 //      Remember to change the IDs at enviroment.properties file
 
 //        Payment Method- Phương thức thanh toán
-        CaptureMoMoResponse captureMoMoResponse = CaptureMoMo.process(environment, orderId, requestId, Long.toString(amount), orderInfo, returnURL, notifyURL, "");
-        System.out.println("xxxPAYURL:  "+captureMoMoResponse.getPayUrl());
-        System.out.println(" ");
-        System.out.println("-------------------------------------------------------------------------------------");
+		CaptureMoMoResponse captureMoMoResponse = CaptureMoMo.process(environment, orderId, requestId,
+				Long.toString(amount), orderInfo, returnURL, notifyURL, "");
+		System.out.println("xxxPAYURL:  " + captureMoMoResponse.getPayUrl());
+		System.out.println(" ");
+		System.out.println("-------------------------------------------------------------------------------------");
 //        Transaction Query - Kiểm tra trạng thái giao dịch
-        QueryStatusTransactionResponse queryStatusTransactionResponse = QueryStatusTransaction.process(environment, orderId, requestId);
-        System.out.println("-------------------------------------------------------------------------------------");
+		QueryStatusTransactionResponse queryStatusTransactionResponse = QueryStatusTransaction.process(environment,
+				orderId, requestId);
+		System.out.println("-------------------------------------------------------------------------------------");
 
 //      Process Payment Result - Xử lý kết quả thanh toán
-        PayGateResponse payGateResponse = PaymentResult.process(environment,new PayGateResponse());
-        System.out.println("-------------------------------------------------------------------------------------");
+		PayGateResponse payGateResponse = PaymentResult.process(environment, new PayGateResponse());
+		System.out.println("-------------------------------------------------------------------------------------");
 
-    }
+	}
 
 }
--- src/main/java/com/xuantujava/controller/web/MomoController.java
+++ src/main/java/com/xuantujava/controller/web/MomoController.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.xuantujava.Momo.allinone.AllInOne;
 import com.xuantujava.repository.UserRepository;
+import com.xuantujava.service.IUserService;
 
 
 
@@ -20,15 +24,43 @@ import com.xuantujava.repository.UserRepository;
 public class MomoController {
 
 	@Autowired
-	UserRepository userRepository;
+	IUserService userService;
+	
+	@RequestMapping(value = "/mox", method = RequestMethod.GET)
+	public ModelAndView homePagex(HttpServletRequest request) {
+		
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		System.out.println("NAME:"+ auth.getName());
+		
+		String userId = userService.findUserIdByUserName(auth.getName().toString()).toString();
+		
+//		System.out.println("xxxxxxxxx");
+		String hold = AllInOne.dump(userId);
+//		System.out.println("url:"+hold);
+
+	
+		ModelAndView mav = new ModelAndView("redirect:"+ hold);
+		return mav;
+	}
+	
+	
+	
 
 	@RequestMapping(value = "/momo1", method = RequestMethod.GET)
 	public ModelAndView homePage(HttpServletRequest request) {
 		
-		
+		System.out.println();
 		
 		
 		System.out.println("1111111111");
+		
+		
+		
+		/////////////// Xu ly them nguoi dung da dang ky
+		
+		
+		
 
 		ModelAndView mav = new ModelAndView("web/momo/momo1");
 		return mav;
