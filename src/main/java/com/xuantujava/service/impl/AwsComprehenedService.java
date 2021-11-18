e65cfa5a75c7da064cb582f4da23fa5713ac8806
TUVX <tuhaha900@gmail.com>
2021-11-18 14:12:05 +0700

    Add Credential AWS

--- src/main/java/com/xuantujava/service/impl/AwsComprehenedService.java
+++ src/main/java/com/xuantujava/service/impl/AwsComprehenedService.java
@@ -2,13 +2,12 @@ package com.xuantujava.service.impl;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-////comprehened imports
-import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.comprehend.AmazonComprehend;
 import com.amazonaws.services.comprehend.AmazonComprehendClientBuilder;
 import com.amazonaws.services.comprehend.model.DetectSentimentRequest;
@@ -18,42 +17,53 @@ import com.amazonaws.services.comprehend.model.DetectSentimentResult;
 public class AwsComprehenedService {
 	
     public static void main(String[] args) throws FileNotFoundException {
-    //	String sentimentComment = AnalyzedOneLineFinal("Wy85Th3Myws").toString();
-//    	BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAUVLNBEMDG2PFDEP2", "vWyWMm6LvfVwgTZOW8EBYc4JyWOpeBSDBrF2fu+w");
-
-//    	com.amazonaws.auth.DefaultAWSCredentialsProviderChain@221af3c0
-
-    	//parsing a CSV file into Scanner class constructor  
-    	Scanner sc = new Scanner(new File("D:\\Red\\Red\\SingelRole_credentials.csv"));  
-    	sc.useDelimiter(",");   //sets the delimiter pattern 
+    	String sentimentComment = AnalyzedOneLineFinal("IT's so good").toString();
     	
-    	int flag = 0;
-    	while (sc.hasNext())  //returns a boolean value  
-    	{  
-    		int x = flag;
-    		System.out.println("Flag:" +flag);
-//    		if( x == 7 )//|| flag == 7)
-    			System.out.println(sc.next());
-//    		if(flag >2000) break;
-    		flag++;
-    	}   
-    	sc.close();  //closes the scanner  
-    	  
-//    	System.out.println(awsCreds);
-    	
+    	System.out.println(sentimentComment);
+
         }
 
 
 
 	public static DetectSentimentResult AnalyzedOneLine(String inputLine) {
 
-		// start comprehened
+// start comprehened
 
 		String text = inputLine;
 
-		AWSCredentialsProvider awsCreds = DefaultAWSCredentialsProviderChain.getInstance();
+//		AWSCredentialsProvider awsCreds = DefaultAWSCredentialsProviderChain.getInstance();
+		Scanner sc;
+		
+		List<String> ArrayCreden = new ArrayList<>();  	
+    	int flag = 0;
+		try {
+			sc = new Scanner(new File("D:\\Red\\Red\\SingelRole_credentials.csv"));
+	    	sc.useDelimiter(",");  
+	    	
+	    	while (flag < 8)  
+	    	{  
+	    		String asd = sc.next().toString();
+	    		if(flag ==6 || flag ==7)
+	    			ArrayCreden.add(asd);
+	    		flag++;
+	    	}   
+	    	sc.close(); 
 
-		AmazonComprehend comprehendClient = AmazonComprehendClientBuilder.standard().withCredentials(awsCreds)
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+
+    	
+    	
+    	
+
+    	BasicAWSCredentials awsCreds = new BasicAWSCredentials(ArrayCreden.get(0), ArrayCreden.get(1));
+    	
+		
+
+		AmazonComprehend comprehendClient = AmazonComprehendClientBuilder.standard()
+				.withCredentials(new AWSStaticCredentialsProvider(awsCreds))
 				.withRegion("ap-northeast-1").build();
 
 		// Call detectSentiment API
